f1834c0c3b5ef5002efa8effcbba9add2b652925
senavarro <serginavarro.ro@gmail.com>
2022-10-05 21:42:04 +0100

    added new characters and event date

--- assets/js/handlers/handlers.js
+++ assets/js/handlers/handlers.js
@@ -11,8 +11,13 @@ const handleForm = () => {
         window.location.href="../../../lily2407.html";
     }else if(userNameValue === 'marthagreen' && pwdValue === 187452){
         window.location.href="../../../marthagreen63428.html";
+    }else if(userNameValue === 'ncage' && pwdValue === 45127331){
+        window.location.href="../../../nathanielcage24601.html";
+    }else if(userNameValue === 'ebirtwistle' && pwdValue === 25149966){
+        window.location.href="../../../eleanorbirtwistle719624.html";
+    }else if(userNameValue === 'jackandeliza' && pwdValue === 3175629){
+        window.location.href="../../../jackcooper&elizaevans15344.html";
     }else{
         alert('Please make sure you have introduced the right credentials')
     }
-    
 }
