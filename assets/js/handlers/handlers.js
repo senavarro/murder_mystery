85f68a65a3ccd2abc36f01c8c805173d33865b4a
senavarro <serginavarro.ro@gmail.com>
2022-10-06 20:03:50 +0100

    new characters added

--- assets/js/handlers/handlers.js
+++ assets/js/handlers/handlers.js
@@ -21,6 +21,18 @@ const handleForm = () => {
         window.location.href="../../../adelaidebrown23156.html";
     }else if(userNameValue === 'eddavies' && pwdValue === 66614901){
         window.location.href="../../../edwin99216.html";
+    }else if(userNameValue === 'vshaw' && pwdValue === 18659017){
+        window.location.href="../../../victorshaw3275.html";
+    }else if(userNameValue === 'fleurlb' && pwdValue === 332218100027){
+        window.location.href="../../../fleur463295.html";
+    }else if(userNameValue === 'idrisd' && pwdValue === 53286921){
+        window.location.href="../../../idrisdogan947612.html";
+    }else if(userNameValue === 'edgarwright' && pwdValue === 6897521){
+        window.location.href="../../../edgarwright82418.html";
+    }else if(userNameValue === 'oscar' && pwdValue === 6129882154){
+        window.location.href="../../../oscarwatson0157.html";
+    }else if(userNameValue === 'adab' && pwdValue === 871255462){
+        window.location.href="../../../adabennett321673.html";
     }else{
         alert('Please make sure you have introduced the right credentials')
     }
